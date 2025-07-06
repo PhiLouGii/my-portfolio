f84f2dc00320a054fe3c5c06387aad43e03496a0
Philippa Giibwa <p.giibwa@alustudent.com>
2025-07-06 16:57:23 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!👋🏾, I’m Philippa Louise Giibwa</h1>
+      <p>Welcome to my portfolio!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Philippa Louise Giibwa</h1>
+      <p>Full-Stack Developer | Visual Artist | Writer</p>
+    </header>
+  );
+};
+
+export default Header;
